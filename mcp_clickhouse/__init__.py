e8611112d63b1001c4fe4328e5ecaae046f6c746
zhaoyun.cloud <zhaoyun.cloud@bytedance.com>
2025-05-09 14:44:47 +0800

    del

--- mcp_clickhouse/__init__.py
+++ mcp_clickhouse/__init__.py
@@ -1,12 +1,10 @@
 from .mcp_server import (
     create_clickhouse_client,
-    list_databases,
     list_tables,
     run_select_query,
 )
 
 __all__ = [
-    "list_databases",
     "list_tables",
     "run_select_query",
     "create_clickhouse_client",
